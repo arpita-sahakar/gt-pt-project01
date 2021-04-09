15bb07a1a5cdac82f1771256b48f4ec59eaada5a
Arpita Saha <arpita.sahaa@gmail.com>
2021-04-09 11:55:31 -0400

    algo 02 unsolved

--- 03-Algorithms/02-longest-word/Unsolved/longest-word.js
+++ 03-Algorithms/02-longest-word/Unsolved/longest-word.js
@@ -1,3 +1,18 @@
 // Write code to create a function that accepts a string (`str`) and returns the longest word in the string
 
-var longestWord = function(str) {};
+// var longestWord = function(str) {};
+
+const longestWord = (str) => {
+  let splitWords = str.split(" ");
+  let findLongestWord = "";
+
+  for (i = 0; i < splitWords.length; i++) {
+      
+    if (splitWords[i].length > findLongestWord.length) {
+      findLongestWord = splitWords[i];
+    }
+  }
+  console.log(findLongestWord);
+};
+
+longestWord("My Name is Arpita Kar");
