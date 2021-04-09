33690c5de52bb5d50ed2df77029db0521e3b83a0
Arpita Saha <arpita.sahaa@gmail.com>
2021-04-09 09:50:31 -0400

    unsolved algo 01

--- 03-Algorithms/01-swap-case/Unsolved/swap-case.js
+++ 03-Algorithms/01-swap-case/Unsolved/swap-case.js
@@ -1,3 +1,19 @@
 // Write code to create a function takes a string and returns the string with all of the letter cases swapped
 
-var swapCase = function(str) {};
+// var swapCase = function(str) {};
+
+const swapCase = str =>{
+    let swap = "";
+    for (i=0; i<str.length; i++){
+
+        if(str[i].toLowerCase() === str[i]){
+            swap = swap + str[i].toUpperCase()
+        } else if( str[i].toUpperCase() === str[i]){
+            swap = swap + str[i].toLowerCase()
+        }
+    }
+    console.log(swap);
+    return swap
+};
+
+const result = swapCase("ArpiTakar")
