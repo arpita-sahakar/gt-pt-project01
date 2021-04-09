c4192ac69e4ad81d42c02ca96c96b13f048a58dc
Arpita Saha <arpita.sahaa@gmail.com>
2021-04-09 12:24:28 -0400

    algo unsolved 03

--- 03-Algorithms/03-acronym-builder/Unsolved/acronym-builder.js
+++ 03-Algorithms/03-acronym-builder/Unsolved/acronym-builder.js
@@ -1,3 +1,16 @@
 // Write code to create a function that accepts a string and returns an acronym for the given string
 
-var acronymBuilder = function(str) {};
+// var acronymBuilder = function(str) {};
+
+var acronymBuilder = (str) => {
+    let splitString = str.split(" ");
+    let acronym = "";
+    for (i=0; i<splitString.length; i++){
+        acronym = acronym + splitString[i][0];
+        // acronym = acronym + splitString[i].slice(0,1);
+    }
+    console.log(acronym);
+
+};
+
+acronymBuilder("see you soon")
